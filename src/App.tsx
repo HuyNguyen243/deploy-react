3ed09fc3cdc561f76f8a60b65aaf621798b5dedd
Huy Nguyễn Dev <huynh.rtbed@gmail.com>
2025-02-10 23:51:26 +0700

    fix: commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />s
-        </a>
+          <img src={viteLogo} className="logo" alt="Vite logo" />s b
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
